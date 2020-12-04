72af52aec10dc46ad16a86bf67e615723381fbd8
axian <74765870+axian-hy@users.noreply.github.com>
2020-12-04 22:43:50 +0100

    Length to .length

---
+++ index.js
@@ -0,0 +1,97 @@
+let fiscalCode = [];
+let vowelRegex = /[aeiou]/gi;
+let consonantRegex =  /[b-df-hj-np-tv-z]/gi;
+
+/* const findVowel = function(str) {
+    let vowels = str.match(/[aeiou]/g);
+    console.log(vowels)
+}
+
+findVowel('zio pane anche se separato')
+
+const findConsonat = function(str) {
+
+} */
+
+/* let firstName = 'mario';
+let vowelRegex = /[aeiou]/g;
+let results = firstName.match(vowelRegex);
+
+if (results.length > 3) {
+    console.log(results.slice(0, 3))
+} */
+
+/* const findVowel = function (str) { 
+    let vowelRegex = /[aeiou]/g;
+    let results = str.match(vowelRegex);
+
+    if (results.length > 3) {
+        let finale = results.slice(0, 3)
+        fiscalCode.push(finale)
+        console.log(fiscalCode);
+    } else {
+        console.log('niente');
+    }
+} */
+
+const lastNameCode = function (lastName) { 
+
+    let vowelArr = lastName.match(vowelRegex);
+    let consonantArr = lastName.match(consonantRegex);
+    let consLength = consonantArr.length;
+    let vowelLength = vowelArr.length;
+
+/*     switch () {
+        case consLength > 3:
+            fiscalCode.push(consonantArr.slice(0,3));
+            break;
+        case consLength == 2 && vowelLength > 0:
+            fiscalCode.push(consonantArr, vowelArr[0]);
+            break;
+        case consLength == 1 && vowelLength > 1:
+            fiscalCode.push(consonantArr, vowelArr.slice(0, 2));
+            break;
+        case consLength == 1 && vowelLength == 1:
+            fiscalCode.push(consonantArr, vowelArr, 'x');
+            break;
+        case consLength == 0 && vowelLength > 1:
+            fiscalCode.push(vowelArr, 'x');
+            break;
+    } */
+
+/*     if (consLength >= 3) {
+        fiscalCode.push(consonantArr.slice(0,3));
+    } else if (consLength == 2 && vowelLength > 0) {
+        fiscalCode.push(consonantArr.slice(0,2), vowelArr[0]);
+    } else if (consLength == 1 && vowelLength > 1) {
+        fiscalCode.push(consonantArr[0], vowelArr.slice(0, 2));
+    } else if (consLength == 1 && vowelLength == 1) {
+        fiscalCode.push(consonantArr[0], vowelArr.slice(0,2), 'x');
+    } else if (consLength == 0 && vowelLength > 1) {
+        fiscalCode.push(vowelArr.slice(0,2), 'x');
+    } */
+
+    if (consonantArr == null && vowelLength > 1) {
+        fiscalCode.push(vowelArr.slice(0,2), 'x');
+    }
+     else if (consLength == 1 && vowelLength == 1) {
+        fiscalCode.push(consonantArr[0], vowelArr[0], 'x');
+    }
+     else if (consLength == 1 && vowelLength > 1) {
+        fiscalCode.push(consonantArr[0], vowelArr.slice(0, 2));
+    }
+     else if (consLength == 2 && vowelLength > 0) {
+        fiscalCode.push(consonantArr.slice(0,2), vowelArr[0]);
+    }
+     else if (consLength >= 3) {
+        fiscalCode.push(consonantArr.slice(0,3));
+    }
+    
+    console.log(vowelArr);
+    console.log(consonantArr);
+    console.log(consLength);
+    console.log(vowelLength);
+    console.log(fiscalCode);
+}
+
+lastNameCode('uu')
