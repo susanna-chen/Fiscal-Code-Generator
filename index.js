fa32125bd8b32f14b5d21098628ce3c6192a9fff
axian <74765870+axian-hy@users.noreply.github.com>
2020-12-12 14:18:44 +0100

    split arr mod

--- index.js
+++ index.js
@@ -8,22 +8,22 @@ const lastNameCode = function (lastName) {
     let consonantArr = lastName.match(consonantRegex);
 
     if (consonantArr == null && vowelArr.length > 1) {
-        fiscalCode.push(vowelArr.slice(0,2), 'x');
+        fiscalCode.push(vowelArr[0], vowelArr[1], 'x');
     }
      else if (vowelArr == null) {
-        console.log('non valid surname');
+        console.log('invalid name');
     }
      else if (consonantArr.length == 1 && vowelArr.length == 1) {
         fiscalCode.push(consonantArr[0], vowelArr[0], 'x');
     }
-     else if (consonantArr.length == 1 && vowevowelArr.lengthlLength > 1) {
-        fiscalCode.push(consonantArr[0], vowelArr.slice(0, 2));
+     else if (consonantArr.length == 1 && vowevowelArr.length > 1) {
+        fiscalCode.push(consonantArr[0], vowelArr[0], vowelArr[1]);
     }
      else if (consonantArr.length == 2 && vowelArr.length > 0) {
-        fiscalCode.push(consonantArr.slice(0,2), vowelArr[0]);
+        fiscalCode.push(consonantArr[0], consonantArr[1], vowelArr[0]);
     }
      else if (consonantArr.length >= 3) {
-        fiscalCode.push(consonantArr.slice(0,3));
+        fiscalCode.push(consonantArr[0], consonantArr[1], consonantArr[2]);
     }
 }
 
@@ -33,42 +33,42 @@ const firstNameCode = function (firstName) {
     let consonantArr = firstName.match(consonantRegex);
 
     if (consonantArr == null || vowelArr == null) {
-        console.log('non valid name');
+        console.log('invalid name');
     }
     else if (consonantArr == null && vowelArr.length > 1) {
-        fiscalCode.push(vowelArr.slice(0,2), 'x');
+        fiscalCode.push(vowelArr[0], vowelArr[1], 'x');
     }
     else if (consonantArr.length == 1 && vowelArr.length == 1) {
         fiscalCode.push(consonantArr[0], vowelArr[0], 'x');
     }
     else if (consonantArr.length == 1 && vowevowelArr.lengthlLength > 1) {
-        fiscalCode.push(consonantArr[0], vowelArr.slice(0, 2));
+        fiscalCode.push(consonantArr[0], vowelArr[0], vowelArr[1]);
     }
     else if (consonantArr.length == 2 && vowelArr.length > 0) {
-        fiscalCode.push(consonantArr.slice(0,2), vowelArr[0]);
+        fiscalCode.push(consonantArr[0], consonantArr[1], vowelArr[0]);
     }
     else if (consonantArr.length >= 3) {
-        fiscalCode.push(consonantArr.slice(0,3));
+        fiscalCode.push(consonantArr[0], consonantArr[1], consonantArr[2]);
     }
     else if (consonantArr.length > 3) {
         fiscalCode.push(consonantArr[0], consonantArr[2], consonantArr[3]);
 
     }
-    console.log(fiscalCode);
+/*     console.log(fiscalCode); */
 }
 
 const birthDateCode = function () { 
     //get code of year
     let date = $('#birth-date').val().split("-");
     let yearResult;
-    console.log(date);
-    let year = date[0] % 100;
+    let year = (date[0] % 100).toString();
     
     if (year < 10) {
         yearResult = "0" + year;
+        fiscalCode.push(yearResult)
+    } else {
+        fiscalCode.push(year)
     }
-    
-    fiscalCode.push(yearResult)
 
     //get code of month
     let month = date[1];
@@ -113,38 +113,66 @@ const birthDateCode = function () {
             break;
     }
 
+    fiscalCode.push(monthResult)
+
     //get day
     let day = date[2];
     let dayResult;
+    let splitDay = day.split('');
 
     if ( $("input[type=radio]:checked").attr('id') == 'male' ) {
-        console.log(dayResult);
+        for ( let i=0; i < splitDay.length; i++ ) {
+            fiscalCode.push(splitDay[i])
+        }
+        fiscalCode.push(day[0], day[1])
     } else if ( $("input[type=radio]:checked").attr('id') == 'female' ) {
-        dayResult = (parseInt(day) + 40).toString()
-        console.log(dayResult);
+        dayResult = (parseInt(day) + 40).toString().split('');
+        $.each(dayResult, function (i, v) { 
+             fiscalCode.push(v)
+        });  
     }
 }
 
 let luoghiNascita;
 
-const getPaesi = $.getJSON( "paese.json", function( data ) {
+$.getJSON( "paese.json", function( data ) {
     luoghiNascita = data;
     $.each(luoghiNascita, function (index, paese) { 
          $('#luogo-nascita').append(`<option value="${paese.denominazione}">${paese.codice}</option>`);
         $('#btn').on('click', function () {
                 if ( $('#birth-place').val().toUpperCase() === paese.denominazione.toUpperCase()) {
-                    fiscalCode.push(paese.codice);
+                    let splitCodice = paese.codice.split('')
+                    for ( let i=0; i < splitCodice.length; i++ ) {
+                        fiscalCode.push(splitCodice[i])
+                    }
                 } else {
                     $('#birth-place').attr('placeholder', 'Insert valid birthplace');
                 }
             });
-
-
     });
     
 });
 
-getPaesi()
+const carattereControlloCode = function () {
+    let splitArr;
+    splitArr = fiscalCode.join('').split('')
+    console.log(splitArr);
+
+}
+
+$('#btn').on('click', function () {
+    let lastNameInputVal = $('#last-name').val();
+    let firstNameInputVal = $('#first-name').val();
+    
+    lastNameCode(lastNameInputVal)
+    firstNameCode(firstNameInputVal)
+    birthDateCode()
+    carattereControlloCode()
+
+    console.log(fiscalCode); 
+
+});
+
 /* getBirthplace()
     .then(data => console.log('resolved:', data))
     .catch(err => console.log('rejected:', err.message)) */
